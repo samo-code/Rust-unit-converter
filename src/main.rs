1b189d69e225d4cc4825e9762cf4422493016146
samo-code <67499163+samo-code@users.noreply.github.com>
2025-07-20 17:38:51 +0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,36 @@
+use clap::Parser;
+use value::{Convert,Convertion};
 
+mod cli;
+mod value;
+
+fn main() {
+    
+        let args = cli::Cli::parse();
+
+        match args.command{
+            cli::Commands::MetersToCms{value} =>{
+                let meters = Convert::meters_to_cms(value);
+               
+                println!("{} m is {} cm",value,meters);
+            },
+            cli::Commands::MetersToKms { value }=>{
+                let meters = Convert::meters_to_kms(value);
+                println!("{} m is {} km",value,meters);
+            },
+            cli::Commands::MilesToKm { value }=>{
+                let miles = Convert::miles_to_km(value);
+                println!("{} miles is {} km",value,miles);
+
+            },
+            cli::Commands::GramsToKgs { value }=>{
+                let grams = Convert::grams_to_kgs(value);
+                println!("{} g is {} kgs",value,grams);
+            },
+            cli::Commands::PoundsToKgs { value }=>{
+                let pounds = Convert::pounds_to_kgs(value);
+                println!("{} lbs is {} kgs",value,pounds);
+            },
+        }
+
+}
